90efc67fa878b9baf027a6831e29756a63ed3333
rihilsanghani <39428870-rihilsanghani@users.noreply.replit.com>
2025-02-06 20:33:15 +0000

    User checkpoint: Add Discord bot functionality using OpenAI API for chat responses.  Includes dependency updates.

---
+++ main.py
@@ -0,0 +1,49 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import os
+from openai import OpenAI
+
+# get secrect token
+token = os.environ['DISCORD_TOKEN']
+openai_token = os.environ['OPEN_AI_API_KEY']
+project_key = os.environ['PROJECT_KEY']
+org_id = os.environ['ORG_ID']
+
+
+client = OpenAI(api_key=openai_token)
+
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user: # if message is from bot
+            return
+            
+        print(f'Message from {message.author}: {message.content}')
+        channel = message.channel
+        
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{
+                "role": "user",
+                "content": message.content
+            }],
+            response_format={"type": "text"},
+            temperature=1,
+            max_completion_tokens=2048,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0)
+        print(response)
+        await channel.send(response.choices[0].message.content)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
